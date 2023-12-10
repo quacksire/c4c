b5f739a6c83fed1e9ce9dedf564e4dcf9819447c
James Hughes <james@jxhug.me>
2023-12-09 19:36:12 -0800

    heatma

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import NavBar from "../components/NavBar";
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -28,6 +29,18 @@ export default function RootLayout({
           <p className='text-gray-400 text-sm'>© {new Date().getFullYear()} ISOaccess</p>
         </div>
       </footer> */}
+
+      <Script strategy="lazyOnload" id="clarity-script">
+        {`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "k3jei3fgfr");
+        `}
+      </Script>
+
+      
 
       </body>
     </html>
